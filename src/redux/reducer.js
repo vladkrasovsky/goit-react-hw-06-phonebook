896d286dc2d2f32435216e21fad381ddaa344cb9
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2023-01-07 10:44:18 +0200

    Adds create reducer function

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,39 +1,32 @@
+import { createReducer } from '@reduxjs/toolkit';
 import { addContact, deleteContact, setNameFilter } from './actions';
 
 const savedContacts = window.localStorage.getItem('contacts');
 
 const contactsInitialState = savedContacts ? JSON.parse(savedContacts) : [];
 
-export const contactsReducer = (state = contactsInitialState, action) => {
-  switch (action.type) {
-    case addContact.type: {
-      const contacts = [...state, action.payload];
-      window.localStorage.setItem('contacts', JSON.stringify(contacts));
-      return contacts;
-    }
-    case deleteContact.type: {
-      const contacts = state.filter(({ id }) => id !== action.payload);
-      window.localStorage.setItem('contacts', JSON.stringify(contacts));
-      return contacts;
-    }
-    default:
-      return state;
-  }
-};
+export const contactsReducer = createReducer(contactsInitialState, {
+  [addContact]: (state, action) => {
+    const contacts = [...state, action.payload];
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    return contacts;
+  },
+  [deleteContact]: (state, action) => {
+    const contacts = state.filter(({ id }) => id !== action.payload);
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    return contacts;
+  },
+});
 
 const filtersInitialState = {
   name: '',
 };
 
-export const filtersReducer = (state = filtersInitialState, action) => {
-  switch (action.type) {
-    case setNameFilter.type: {
-      return {
-        ...state,
-        name: action.payload,
-      };
-    }
-    default:
-      return state;
-  }
-};
+export const filtersReducer = createReducer(filtersInitialState, {
+  [setNameFilter]: (state, action) => {
+    return {
+      ...state,
+      name: action.payload,
+    };
+  },
+});
